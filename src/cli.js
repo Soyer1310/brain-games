a589771bfee786c0b16755274a389a96d6e2ee00
Soyer1310 <soyer131313@gmail.com>
2024-11-29 21:23:41 +0300

    add sayHello function

---
+++ src/cli.js
@@ -0,0 +1,14 @@
+import readlineSync from 'readline-sync';
+
+const getUserName = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  return userName;
+}
+
+const sayHello = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = getUserName();
+  console.log(`Hello, ${userName}!`);
+}
+
+export default sayHello;
