3f479f21e04045e0d2f372ae649892933b832c35
Soyer1310 <soyer131313@gmail.com>
2024-12-01 19:50:23 +0300

    fix(eslint): add "quotes": "off"

--- eslint.config.js
+++ eslint.config.js
@@ -48,6 +48,7 @@ export default [
       'import/no-named-as-default-member': 'off',
       'no-console': 'off',
       'import/no-extraneous-dependencies': 'off',
+      'quotes': 'off',
     },
   },
 ];
