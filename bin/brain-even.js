e9cbfd830e894e4fe92fa164e42a088499b95c5f
Soyer1310 <soyer131313@gmail.com>
2024-12-02 18:04:21 +0300

    feat(brain-even): separate functionality

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,10 +1,5 @@
 #!/usr/bin/env node
-import sayHello from '../src/cli.js';
-import brainEven, { task } from '../src/brain-even.js';
-import getUserName from '../src/get-user-name.js';
-import playGame from '../src/playGame.js';
+import brainEven, { task } from '../games/brain-even.js';
+import index from '../src/index.js';
 
-console.log('Welcome to the Brain Games!');
-const userName = getUserName();
-sayHello(userName);
-playGame(brainEven, userName, task);
+index(brainEven, task);
